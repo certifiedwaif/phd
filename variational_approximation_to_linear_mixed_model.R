31419e8090d605b251fb8d60bec6c47caea8ddb3
Mark Greenaway <markg@maths.usyd.edu.au>
2013-08-26 14:17:53 +1000

    This works a lot better, but the log likelihood still goes backwards.

--- variational_approximation_to_linear_mixed_model.R
+++ variational_approximation_to_linear_mixed_model.R
@@ -4,16 +4,16 @@ require(Matrix)
 
 # Get Orthodont data
 
-logLik = function()
+calcLogLik = function()
 {
   logLik = 0
   logLik = logLik + 0.5*(p + sum(K))- 0.5*n*log(2*pi) - 0.5*p * log(sigma2_beta)
-  logLik = logLik + 0.5 * log(det(Sigma_q_beta_u)) - (sum(mu_q_beta^2) + trace(Sigma_q_beta)) / (2*sigma2_beta)
+  logLik = logLik + 0.5 * log(det(Sigma_q_beta_u)) - (sum(mu_q_beta_u^2) + trace(Sigma_q_beta_u)) / (2*sigma2_beta)
   logLik = logLik + A_epsilon * log(B_epsilon) - (A_epsilon + 0.5 * n) * log(B_q_sigma2_epsilon)
   logLik = logLik + lgamma(A_epsilon + 0.5*n) - lgamma(A_epsilon)
-  logLik = logLik + sum(A_ul * log(B_ul) - (A_ul + 0.5*K_l) * log(B_q_sigma2_ul) + lgamma(A_ul + 0.2K_l) - lgamma(A_ul))
+  logLik = logLik + sum(A_u * log(B_u) - (A_u + 0.5*K) * log(B_q_sigma2_u) + lgamma(A_u + 0.5 * K) - lgamma(A_u))
 
-  return logLik
+  return(logLik)
 }
 
 trace = function(X)
@@ -31,33 +31,48 @@ eye = function(p)
 # beta_i's all independently N(0, sigma2_beta), sigma2_u, sigma2_epsilon independently ~ IG(A, B)
 
 
-X = matrix(c(1, 1,
-             1, 2,
-             2, 3,
-             4, 5), 4, 2)
+X = matrix(c(1, 0,
+             1, 1,
+             1, 0,
+             1, 1), 4, 2, byrow=TRUE)
 Z = matrix(c(1, 0,
              1, 0,
              0, 1,
-             0, 1), 4, 2)
-Y = matrix(c(9, 8, 7, 6), 4, 1)
+             0, 1), 4, 2, byrow=TRUE)
+Y = matrix(c(9, 8, 9, 8), 4, 1)
 C = cbind(X, Z)
-A_epsilon = 1
-A_u = 1
-B_epsilon = 1
-B_u = 1
+A_epsilon = 1/100
+A_u = 1/100
+B_epsilon = 1/100
+B_u = 1/100
 K = 2
 sigma2_beta = 5
 p = 2
-
+n = 4
 # Initialise
 B_q_sigma2_epsilon = 1
 B_q_sigma2_u = 1
 
 # Cycle
-Sigma_q_beta_u_inv = (A_epsilon + 0.5*n)/(B_q_sigma2_epsilon) * t(C) %*% C
-Sigma_q_beta_u_inv = Sigma_q_beta_u_inv + bdiag(1/sigma2_beta * eye(p), (A_u + 0.5 * K)/(B_q_sigma2_u) * eye(K))
-Sigma_q_beta_u = solve(Sigma_q_beta_u_inv)
-mu_q_beta_u = Sigma_q_beta_u %*% t(C) %*% Y
-B_q_sigma2_epsilon = B_epsilon + 0.5*(sum((Y - C %*% mu_q_beta_u)^2) + trace(t(C) %*% C %*% Sigma_q_beta_u))
-B_q_sigma2_ul = B_u + 0.5 * (sum(mu_q_u^2) + trace(Sigma_q_u))
-print(logLik)
+lastLogLik = -1e6
+logLik = -1e6 + 1
+while (logLik > lastLogLik)
+{
+  lastLogLik = logLik
+  Sigma_q_beta_u_inv = (A_epsilon + 0.5*n)/(B_q_sigma2_epsilon) * t(C) %*% C
+  Sigma_q_beta_u_inv = Sigma_q_beta_u_inv + bdiag(eye(p) / sigma2_beta, (A_u + 0.5 * K)/(B_q_sigma2_u) * eye(K))
+  #eigen(Sigma_q_beta_u_inv)
+  Sigma_q_beta_u = solve(Sigma_q_beta_u_inv)
+  mu_q_beta_u = (A_epsilon + 0.5 * n)/(B_q_sigma2_epsilon) * Sigma_q_beta_u %*% t(C) %*% Y
+  #mu_q_beta_u
+  B_q_sigma2_epsilon = B_epsilon + 0.5*(sum((Y - C %*% mu_q_beta_u)^2) + trace(t(C) %*% C %*% Sigma_q_beta_u))
+  B_q_sigma2_u = B_u + 0.5 * (sum(mu_q_beta_u[3:4]^2) + trace(Sigma_q_beta_u[3:4, 3:4]))
+  logLik = calcLogLik()
+  
+  params = list(Sigma_q_beta_u, mu_q_beta_u, B_q_sigma2_epsilon, B_q_sigma2_u)
+  print(params)
+  print(logLik)
+  print(logLik - lastLogLik)
+}
+
+# Compare against MCMC result?
