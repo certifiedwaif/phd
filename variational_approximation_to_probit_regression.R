40a2ddde683e5a7a8ca2eb8b160a3e455c79a999
Mark Greenaway <markg@maths.usyd.edu.au>
2013-09-16 12:38:09 +1000

    Put model fitting in a loop, and adding debugging prints.

--- variational_approximation_to_probit_regression.R
+++ variational_approximation_to_probit_regression.R
@@ -16,9 +16,28 @@ n = 4
 p = 2
 Sigma_beta = diag(rep(1, p))
 mu_beta = as.matrix(c(0, 0), byrow=TRUE)
-# Initialise ----
-mu_q_a = rep(0, 4)
-# Cycle ----
-mu_q_beta = solve(t(X) %*% X + solve(Sigma_beta)) %*% (t(X) %*% mu_q_a + solve(Sigma_beta) %*% mu_beta)
-mu_q_a = X %*% mu_q_beta + (dnorm(X %*% mu_q_beta))/(pnorm(X %*% mu_q_beta)^y * pnorm(X %*% mu_q_beta) - rep(1, n))^(rep(1, n) - y)
-logLik(y, mu_q_a, mu_q_beta, X, Sigma_beta, p)
+
+fit_probit_model = function(y, X, n, p, Sigma_beta, mu_beta)
+{
+  # Initialise ----
+  mu_q_a = rep(0, 4)
+  
+  # Cycle ----
+  lastLogLik = -Inf
+  likDiff = Inf
+  iteration = 0
+  while (likDiff > 1e-8) {
+    mu_q_beta = solve(t(X) %*% X + solve(Sigma_beta)) %*% (t(X) %*% mu_q_a + solve(Sigma_beta) %*% mu_beta)
+    mu_q_a = X %*% mu_q_beta + (dnorm(X %*% mu_q_beta))/(pnorm(X %*% mu_q_beta)^y * pnorm(X %*% mu_q_beta) - rep(1, n))^(rep(1, n) - y)
+    lik = logLik(y, mu_q_a, mu_q_beta, X, Sigma_beta, p)
+    if (lik>lastLogLik) {
+      likDiff = lik - lastLogLik
+      lastLogLik = lik
+    }
+    iteration = iteration +1
+    print(list(iteration=iteration, lik=lik, likDiff=likDiff))
+  }
+  
+  return(list(mu_q_beta=mu_q_beta, mu_q_a=mu_q_a))
+}
+fit_probit_model(y, X, n, p, Sigma_beta, mu_beta)
