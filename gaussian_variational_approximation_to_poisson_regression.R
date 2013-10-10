7486ee15750d962a08f3fe0c1cc20d4f703419b6
Mark Greenaway <certifiedwaif@gmail.com>
2013-10-10 22:35:02 +1100

    Re-organise code to use reusable functions.

--- gaussian_variational_approximation_to_poisson_regression.R
+++ gaussian_variational_approximation_to_poisson_regression.R
@@ -1,4 +1,7 @@
+source("fit_gaussian_variational_approximation_to_poisson_regression.R")
+
 # Example data ----
+df = faithful
 n = 272
 p = 2
 y = faithful$waiting
@@ -9,25 +12,4 @@ mubeta = rep(0, p)
 Sigmabeta = diag(rep(1, p))
 prior_sigma2 = 1e7
 
-# Initialise mean field update parameters ----
-# FIXME - this initialisation totally doesn't work
-muqbeta = lm(log(waiting)~eruptions, data=faithful)$coef
-#fit = glm(waiting~eruptions, family=poisson, data=faithful)
-#muqbeta = coef(fit)
-
-# Cycle mean field update equations until convergence ----
-wv = exp(X %*% muqbeta)
-W <- diag(c(wv), n, n)
-Sigmaqbeta <- solve(t(X) %*% W %*% X +  prior_sigma2^(-1) * diag(rep(1, p)))
-
-dmuqbeta = t(X) %*% y
-# FIXME ----
-dmuqbeta = dmuqbeta - t(X) %*% exp(X %*% muqbeta + .5 * diag(X %*% Sigmaqbeta %*% t(X)))
-dmuqbeta = dmuqbeta - solve(Sigmabeta) %*% (muqbeta - mubeta)
-
-muqbeta <- muqbeta + Sigmaqbeta %*% dmuqbeta
-
-# Log likelihood ----
-loglik = t(y) %*% X %*% muqbeta - t(rep(1, n)) %*% exp(X %*% muqbeta + .5 * diag(X %*% Sigmaqbeta %*% t(X)))
-loglik = loglik - .5 * t(mubeta - muqbeta) %*% solve(Sigmabeta) %*% (mubeta - muqbeta)
-loglik = loglik - .5 * p * log(2 * pi) - .5 * log(det(Sigmaqbeta)) - t(rep(1, n)) %*% lgamma(y + 1)
+fit_poisson_regression(y=y, X=X, df=df, mubeta=mubeta, Sigmabeta=Sigmabeta, prior_sigma2=prior_sigma2)
