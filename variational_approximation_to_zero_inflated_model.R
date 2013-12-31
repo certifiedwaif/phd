84a69a0a5ecdfce4b19d5e9fef8fa296df252f64
Mark Greenaway <markg@maths.usyd.edu.au>
2014-01-01 07:20:35 +1100

    Refactor code into functions, with a main() function calling all others. Check the accuracy of the approximation for a range of parameter values.

--- variational_approximation_to_zero_inflated_model.R
+++ variational_approximation_to_zero_inflated_model.R
@@ -1,28 +1,24 @@
 # variational_approximation_to_zero_inflated_model.R
-require(parallel)
 
-# I really have to come up with a better way of naming these ----
-
-# Simulate the data ----
-rho = .5
-lambda = 100
-n = 1000
-x = rep(NA, n)
-for (i in 1:n) {
-  if (runif(1) >= rho) {
-    x[i] = rpois(1, lambda)
-  } else {
-    x[i] = 0
+generate_test_data = function (n, rho, lambda)
+{
+  x = rep(NA, n)
+  for (i in 1:n) {
+    if (runif(1) >= rho) {
+      x[i] = rpois(1, lambda)
+    } else {
+      x[i] = 0
+    }
   }
+  return(x)
 }
-a = 10
-b = 10
-# MCMC ----
+
 logit = function(p) log(p/(1-p))
 expit = function(eta) 1/(1+exp(-eta))
-zero_infl_mcmc = function(iterations)
+zero_infl_mcmc = function(iterations, x, a, b)
 {
   # Initialise
+  n = length(x)
   lambda = rep(NA, iterations+1)
   rho = rep(NA, iterations+1)
   lambda[1] = mean(x)
@@ -38,9 +34,12 @@ zero_infl_mcmc = function(iterations)
     arg = exp(-lambda[i] + logit(rho[i]))
     eta = arg/(1 + arg)
     
+    # The following code is correct, but 100 times slower as R is interpreted.
+    # So we use the vectorised code instead.
     #for (j in 1:length(zero_observation_idx)) {
     #  r[zero_observation_idx[j]] = rbinom(1, 1, eta)
     #}
+    
     r[zero_observation_idx] = rbinom(length(zero_observation_idx), 1, eta) 
     lambda[i+1] = rgamma(1, a + sum(x), b + sum(r))
     rho[i+1] = rbeta(1, sum(r) + 1, n - sum(r) + 1)
@@ -48,24 +47,9 @@ zero_infl_mcmc = function(iterations)
   return(list(lambda=lambda, rho=rho))
 }
 
-iterations = 1e6
-burnin = 1e3
-
-start = Sys.time()
-result_mcmc = zero_infl_mcmc(iterations+burnin)
-Sys.time() - start
-# Throw away burn-in samples.
-# Brackets turn out to be incredibly important here!!!
-result_mcmc$rho = result_mcmc$rho[(burnin+1):(burnin+iterations+1)]
-result_mcmc$lambda = result_mcmc$lambda[(burnin+1):(burnin+iterations+1)]
-
-# FIXME: Why are there NAs?
-# 1000 iterations in 0.07461715 seconds.
-
-# Variational approximation ----
-# Initialise ----
-zero_infl_var = function(iterations)
+zero_infl_var = function(iterations, x, a, b)
 {
+  n = length(x)
   p = rep(sum(x == 0)/length(x), n)
   eta = rep(NA, n)
   a_lambda = a + sum(x)
@@ -89,29 +73,73 @@ zero_infl_var = function(iterations)
   }
   return(params)
 }
-start = Sys.time()
-result_var = zero_infl_var(10)
-Sys.time() - start
-# Variational approximation takes .05 seconds to run 10 iterations. So 5ms per iteration,
-# or 200 iterations a second.
-result_var$a_lambda / result_var$b_lambda
-result_var$a_rho / (result_var$a_rho + result_var$b_lambda)
 
 # Calculate accuracy ----
 # Approximate the L1 norm between the variational approximation and
 # the MCMC approximation
-density_mcmc_rho = density(result_mcmc$rho)
-integrand = function(x)
+calculate_accuracy = function(result_mcmc, result_var)
 {
-  fn = splinefun(density_mcmc_rho$x, density_mcmc_rho$y)
-  return(abs(fn(x) - dbeta(x, result_var$a_rho, result_var$b_rho)))
+  density_mcmc_rho = density(result_mcmc$rho)
+  integrand = function(x)
+  {
+    fn = splinefun(density_mcmc_rho$x, density_mcmc_rho$y)
+    return(abs(fn(x) - dbeta(x, result_var$a_rho, result_var$b_rho)))
+  }
+  integrate(integrand, min(density_mcmc_rho$x), max(density_mcmc_rho$x), subdivisions = length(density_mcmc_rho$x))
+  
+  density_mcmc_lambda = density(result_mcmc$lambda)
+  integrand = function(x)
+  {
+    fn = splinefun(density_mcmc_lambda$x, density_mcmc_lambda$y)
+    return(abs(fn(x) - dgamma(x, result_var$a_lambda, result_var$b_lambda)))
+  }
+  result = integrate(integrand, min(density_mcmc_lambda$x), max(density_mcmc_lambda$x), subdivisions = length(density_mcmc_lambda$x))
+  accuracy = 1 - .5 * result$value
+  return(accuracy)
 }
-integrate(integrand, min(density_mcmc_rho$x), max(density_mcmc_rho$x), subdivisions = length(density_mcmc_rho$x))
 
-density_mcmc_lambda = density(result_mcmc$lambda)
-integrand = function(x)
+check_accuracy = function(n, rho, lambda)
 {
-  fn = splinefun(density_mcmc_lambda$x, density_mcmc_lambda$y)
-  return(abs(fn(x) - dgamma(x, result_var$a_lambda, result_var$b_lambda)))
+  x = generate_test_data(n, rho, lambda)
+  
+  a = 10
+  b = 10
+  
+  # MCMC ----
+  iterations = 1e6
+  burnin = 1e3
+  
+  start = Sys.time()
+  result_mcmc = zero_infl_mcmc(iterations+burnin, x, a, b)
+  mcmc_runtime = Sys.time() - start
+  # Throw away burn-in samples.
+  # Brackets turn out to be incredibly important here!!!
+  result_mcmc$rho = result_mcmc$rho[(burnin+1):(burnin+iterations+1)]
+  result_mcmc$lambda = result_mcmc$lambda[(burnin+1):(burnin+iterations+1)]
+  # 1000 iterations in 0.07461715 seconds.
+  
+  # Variational approximation ----
+  start = Sys.time()
+  result_var = zero_infl_var(10, x, a, b)
+  var_runtime = Sys.time() - start
+  # Variational approximation takes .05 seconds to run 10 iterations. So 5ms per iteration,
+  # or 200 iterations a second.
+  var_lambda = result_var$a_lambda / result_var$b_lambda
+  var_rho = result_var$a_rho / (result_var$a_rho + result_var$b_lambda)
+  
+  accuracy = calculate_accuracy(result_mcmc, result_var)
+  return(list(n=n, rho=rho, lambda=lambda, accuracy=accuracy))
 }
-integrate(integrand, min(density_mcmc_lambda$x), max(density_mcmc_lambda$x), subdivisions = length(density_mcmc_lambda$x))
+
+# Check accuracy of the approximation for a range of parameter values ----
+main = function()
+{
+  n = 1000
+  rho = .5
+  lambda = 100
+  
+  for (rho in .1*1:9)
+    for (lambda in seq(5, 100, 5))
+      print(check_accuracy(n, rho, lambda))
+}
+main()
