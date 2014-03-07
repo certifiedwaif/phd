560df2195108aeefe4f23b166e2d7dfcfef4801a
Mark Greenaway <markg@maths.usyd.edu.au>
2014-03-07 11:02:11 +1100

    PDF of lower bound convergence for univariate model

--- code/run_zero_inflated_model.R
+++ code/run_zero_inflated_model.R
@@ -3,6 +3,7 @@
 # Variational approximation
 # Check that lower bounds are monotonically increasing
 # Compare accuracy against MCMC
+setwd("~/phd/code")
 source("zero_inflated_model.R")
 n = 10000
 rho = .5
@@ -15,7 +16,9 @@ b = 0.01
 
 # Variational approximation ----
 start = Sys.time()
+#pdf("lower_bound_convergence.pdf")
 result_var = zero_infl_var(x, a, b)
+#dev.off()
 var_runtime = Sys.time() - start
 # Variational approximation takes .05 seconds to run 10 iterations. So 5ms per iteration,
 # or 200 iterations a second.
