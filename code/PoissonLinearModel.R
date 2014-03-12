1ff992768e2d327231a9c7fbfa9aae4e7243dc56
Mark Greenaway <certifiedwaif@gmail.com>
2014-03-12 12:37:03 +1100

    Comment John's code, in preparation for modifying it to include fixed effects.

--- code/PoissonLinearModel.R
+++ code/PoissonLinearModel.R
@@ -7,9 +7,6 @@ require(Matrix)
 
 f.lap <- function(vbeta,vu,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv) 
 {       
-    #veta <- mX%*%vbeta + mZ%*%vu
-    #f <- sum(vy*veta - exp(veta)) - 0.5*t(vbeta)%*%mSigmaBeta.inv%*%vbeta - 0.5*t(vu)%*%mSigma.inv%*%vu 
-    #return(f)
     vtheta <- c(vbeta, vu)
     d <- length(vtheta)
     veta <- cbind(mX, mZ)%*%vtheta
@@ -24,35 +21,16 @@ vg.lap <- function(vbeta,vu,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv)
   vtheta <- c(vbeta, vu)
   vg <- t(cbind(mX, mZ))%*%(vy - exp(cbind(mX, mZ)%*%vtheta)) - rbind(mSigmaBeta.inv%*%vbeta, mSigma.inv%*%vu)
   return(vg)
-    #veta <- mX%*%vbeta + mZ%*%vu
-    #vg <- t(cbind(mX,mZ))%*%(vy - exp(veta)) - rbind(mSigmaBeta.inv%*%vbeta, mSigma.inv%*%vu)
-    #return(vg)
 }
 
 ###############################################################################
 
 mH.lap <- function(vbeta,vu,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv) 
 {
-  #vtheta <- c(vbeta, vu)
-  #vw <- exp(cbind(mX, mZ)%*%vtheta); dim(vw) <- NULL
-  #mH <- -t(cbind(mX, mZ)*vw)%*%cbind(mX, mZ) - bdiag(mSigmaBeta.inv, mSigma.inv)
   mW <- diag(as.vector(exp(mX%*%vbeta + mZ%*%vu)))
-  #mH <- bdiag(-t(mX) %*% mW %*% mX - mSigmaBeta.inv,
-  #            -t(mZ) %*% mW %*% mZ - mSigma.inv)
   mH <- rbind(cbind(-t(mX)%*%mW%*%mX - mSigmaBeta.inv, -t(mX)%*%mW%*%mZ),
               cbind(-t(mZ)%*%mW%*%mX, -t(mZ)%*%mW%*%mZ - mSigma.inv))
   return(mH)
-  #diagMat <- function(x, y)
-  #{
-  #  result = matrix(0, nrow(x) + nrow(y), ncol(x) + ncol(y))
-  #  result[1:nrow(x), 1:ncol(x)] = x
-  #  result[(nrow(x)+1):(nrow(x)+nrow(y)), (ncol(x)+1):(ncol(x)+ncol(y))]
-  #  return(result)
-  #}
-	#veta = mX%*%vbeta + mZ%*%vu
-  #  vw <- exp(veta); dim(vw) <- NULL
-  #  mH <- -t(cbind(mX,mZ)*vw)%*%cbind(mX,mZ) - diagMat(mSigmaBeta.inv, mSigma.inv)
-  #  return(mH)
 }
 
 ###############################################################################
@@ -85,14 +63,18 @@ fit.Lap <- function(vbeta,vu,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv, debug=FALSE)
         
     #vu <- res$par
     #mLambda <- solve(-mH.lap(vu,vy,mZ,mSigma.inv),tol=1.0E-99)
+
+	# FIXME: The term at the end involving determinants is probably wrong.
     f <- f.lap(vbeta,vu,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv) + 0.5*log(det(mLambda%*%bdiag(mSigmaBeta.inv, mSigma.inv)))
-    print(f)
+    if (debug)
+		print(f)
     return(list(vmu=vmu,mLambda=mLambda,f=f))
 }
 
 ###############################################################################
 
 f.G <- function(vmu,mLambda,vy,mZ,mSigma.inv,gh) 
+# Calculate the main part of the lower bound.
 {
     d <- length(vmu)
     
@@ -108,16 +90,20 @@ f.G <- function(vmu,mLambda,vy,mZ,mSigma.inv,gh)
 ###############################################################################
 
 f.GVA <- function(vu,vy,mZ,mSigma.inv,gh,mR,Rinds,Dinds)
+# Calculate lower bound.
 {
+	# Unpack vmu and mR from vu
    d <- ncol(mZ)  
    vmu <- vu[1:d]
    mR[Rinds] <- vu[(1+d):length(vu)]
    mR[Dinds] <- exp(mR[Dinds]) 
+	# Threshold entries in mR at 10,000
     for (i in 1:length(Dinds)) {
         mR[Dinds[i]] <- min(c(1.0E5,mR[Dinds[i]]))
     }   
    mLambda <- mR%*%t(mR)   
    
+	# Calculate f
    f <- sum(log(diag(mR))) + f.G(vmu,mLambda,vy,mZ,mSigma.inv,gh) 
    f <- f + 0.5*d*log(2*pi) + 0.5*d
    
@@ -149,6 +135,7 @@ mH.G <- function(vmu,mLambda,vy,mZ,mSigma.inv,vB2)
 vg.GVA.approx <- function(vu,vy,mZ,mSigma.inv,gh,mR,Rinds,Dinds)
 {
     n <- length(vy); P <- length(vu); d <- ncol(mZ)
+		# Question: Why do we do this epsilon averaging trick?
 		eps <- 1.0E-6
 		f <- f.GVA(vu,vy,mZ,mSigma.inv,gh,mR,Rinds,Dinds)
 		vg.approx <- matrix(0,P,1)
@@ -180,10 +167,13 @@ vg.GVA.approx <- function(vu,vy,mZ,mSigma.inv,gh,mR,Rinds,Dinds)
 
 vg.GVA <- function(vu,vy,mZ,mSigma.inv,gh,mR,Rinds,Dinds)
 {
+	# FIXME: Bug? vmu is not in scope
+	# d <- ncol(mZ)
     d <- length(vmu)
     vmu <- vu[1:d]
     mR[Rinds] <- vu[(1+d):length(vu)]
     mR[Dinds] <- exp(mR[Dinds]) 
+	# Threshold entries of mR at 1,000
     for (i in 1:length(Dinds)) {
         mR[Dinds[i]] <- min(c(1.0E3,mR[Dinds[i]]))
     }    
@@ -195,6 +185,7 @@ vg.GVA <- function(vu,vy,mZ,mSigma.inv,gh,mR,Rinds,Dinds)
     vB1 <- res.B12$vB1
     vB2 <- res.B12$vB2
     
+	# Set the derivative of vmu in the first d entries of vg
     vg <- 0*vu
     vg[1:d] <- vg.G(vmu,mLambda,vy,mZ,mSigma.inv,vB1) 
 
@@ -214,6 +205,7 @@ vg.GVA <- function(vu,vy,mZ,mSigma.inv,gh,mR,Rinds,Dinds)
     #    }      
     #}
     
+	# Set the derivative of mR in the last entries of vg
     dmLambda[Dinds] <- dmLambda[Dinds]*mR[Dinds]
     vg[(1+d):length(vu)] <- dmLambda[Rinds]    
    
