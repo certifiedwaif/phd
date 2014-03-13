b777a60a23e0a47e6ee268083a914176bbcad459
Mark Greenaway <markg@maths.usyd.edu.au>
2014-03-13 15:09:17 +1100

    Re-organise the file to run the zero-inflated Poisson variational approx. code into a test.

---
+++ code/test_zero_inflated_model.R
@@ -0,0 +1,31 @@
+# Test variational approximation. Unit tests? ----
+# Simulate data
+# Variational approximation
+# Check that lower bounds are monotonically increasing
+# Compare accuracy against MCMC
+source("zero_inflated_model.R")
+require(testthat)
+
+test_univariate_zip <- function()
+{
+	n = 1000
+	expected_rho = .5
+	expected_lambda = 1
+
+	vx = generate_test_data(n, expected_rho, expected_lambda)
+
+	a_lambda = 0.01
+	b_lambda = 0.01
+
+	result_var = zero_infl_var(vx, a_lambda, b_lambda)
+
+	expect_equal(result_var$a_lambda / result_var$b_lambda, expected_lambda, tolerance=1e-6)
+	expect_equal(result_var$a_rho / (result_var$a_rho + result_var$b_rho), expected_rho, tolerance=1e-6)
+}
+
+#main_check_accuracy()
+main <- function()
+{
+	test_univariate_zip()
+}
+main()
