20e5d0e925ac84540b2b84f389b70f17eafcad9f
Mark Greenaway <markg@maths.usyd.edu.au>
2014-04-03 17:01:11 +1100

    Revert to John's original code, then modify the Laplace approximation to incorporate chances for zero-inflation. This is much simpler!

--- code/zero_inflated_poisson_linear_model.R
+++ code/zero_inflated_poisson_linear_model.R
@@ -1,120 +1,98 @@
-# FIXME: Extend this code to the zero-inflated case
-
+# John Ormerod's Poisson regression code, slightly modified to be used by my
+# zero-inflated model code.
 ###############################################################################
 
 source("CalculateB.R")
-require(Matrix)
 
 ###############################################################################
 
-f.lap.zero <- function(vbeta,vu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv) 
+f.lap <- function(vtheta,vy,vr,mX,mSigma.inv,mLambda) 
 {       
-    vtheta <- c(vbeta, vu)
     d <- length(vtheta)
-    veta <- cbind(mX, mZ)%*%vtheta
-    f <- sum(vr*vy*veta - vr*exp(veta)) - 0.5*(t(vbeta)%*%mSigmaBeta.inv%*%vbeta + t(vu)%*%mSigma.inv%*%vu)
+    veta <- mX%*%vtheta
+    f <- sum(vy*veta - vr*exp(veta+.5*diag(mX%*%mLambda*t(mX))) - 0.5*t(vtheta)%*%mSigma.inv%*%vtheta - 0.5*tr(mLambda%*%Dhat)
     return(f)
 }
 
 ###############################################################################
 
-vg.lap.zero <- function(vbeta,vu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv) 
+vg.lap <- function(vtheta,vy,vr,mX,mSigma.inv,mLambda) 
 {       
-  vtheta <- c(vbeta, vu)
-  vg <- t(cbind(mX, mZ))%*%(vr*vy - vr*exp(cbind(mX, mZ)%*%vtheta)) - rbind(mSigmaBeta.inv%*%vbeta, mSigma.inv%*%vu)
-  return(vg)
+    vg <- t(mX)%*%(vy - vr*exp(mX%*%vtheta+.5*diag(mX%*%mLambda*t(mX)))) - mSigma.inv%*%vtheta
+    return(vg)
 }
 
 ###############################################################################
 
-mH.lap.zero <- function(vbeta,vu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv) 
+mH.lap <- function(vtheta,vy,vr,mX,mSigma.inv,mLambda) 
 {
-	# FIXME: Is this updated for zero-inflation?
-  mW <- diag(as.vector(exp(mX%*%vbeta + mZ%*%vu)))
-  mH <- rbind(cbind(-t(mX)%*%mW%*%mX - mSigmaBeta.inv, -t(mX)%*%mW%*%mZ),
-              cbind(-t(mZ)%*%mW%*%mX, -t(mZ)%*%mW%*%mZ - mSigma.inv))
-  return(mH)
+    vw <- exp(mX%*%vtheta+.5*diag(mX%*%mLambda*t(mX))); dim(vw) <- NULL
+    mH <- -t(mX*vw)%*%mX - mSigma.inv
+    return(mH)
 }
 
 ###############################################################################
 
-fit.Lap.zero <- function(vbeta,vu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv, debug=FALSE) 
+fit.Lap <- function(vmu,vy,vr,mX,mSigma.inv,mLambda) 
 {
     MAXITER <- 100
-    vmu = c(vbeta, vu)
     
     for (ITER in 1:MAXITER) {
-        f  <- f.lap.zero(vbeta,vu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv) 
-        vg <- vg.lap.zero(vbeta,vu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv) 
-        mH <- mH.lap.zero(vbeta,vu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv)
+        f  <- f.lap(vmu,vy,vr,mX,mSigma.inv,mLambda)
+        vg <- vg.lap(vmu,vy,vr,mX,mSigma.inv,mLambda)
+        mH <- mH.lap(vmu,vy,vr,mX,mSigma.inv,mLambda)
         mLambda <- solve(-mH,tol=1.0E-99)
         vmu <- vmu + mLambda%*%vg
-		# FIXME: This code isn't general.
-        vbeta <- vmu[1:2]
-        vu <- vmu[3:4]
-        if (debug)
-            print(c(ITER,f,max(abs(vg))))
+        #print(c(ITER,f,max(abs(vg))))
         if (max(abs(vg))<1.0E-8) {
             break;
         }
     } 
 
-    #vu <- vmu
+    #vtheta <- vmu
     #controls <- list(maxit=1000,trace=0,fnscale=-1,REPORT=1,reltol=1.0e-15)
-    #res <- optim(par=vu, fn=f.lap, gr=vg.lap,
+    #res <- optim(par=vtheta, fn=f.lap, gr=vg.lap,
     #    method="BFGS",lower=-Inf, upper=Inf, control=controls,
-    #    vy=vy,mZ=mZ,mSigma.inv) 
+    #    vy=vy,mX=mX,mSigma.inv) 
         
-    #vu <- res$par
-    #mLambda <- solve(-mH.lap(vu,vy,mZ,mSigma.inv),tol=1.0E-99)
-
-	# FIXME: The term at the end involving determinants is probably wrong.
-    f <- f.lap.zero(vbeta,vu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv) + 0.5*log(det(mLambda%*%bdiag(mSigmaBeta.inv, mSigma.inv)))
-    if (debug)
-		print(f)
+    #vtheta <- res$par
+    #mLambda <- solve(-mH.lap(vtheta,vy,mX,mSigma.inv),tol=1.0E-99)
+    
+    f <- f.lap(vmu,vy,vr,mX,mSigma.inv,mLambda) + 0.5*log(det(mLambda%*%mSigma.inv))
+    print(f)
     return(list(vmu=vmu,mLambda=mLambda,f=f))
 }
 
 ###############################################################################
 
-f.G <- function(vmu,mLambda,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,gh) 
-# Calculate the main part of the lower bound.
+f.G <- function(vmu,mLambda,vy,mX,mSigma.inv,gh) 
 {
-    d1 <- ncol(mX)
-	d2 <- ncol(mZ)
-	vbeta <- vmu[1:d1]
-	vu <- vmu[(d1+1):(d1+d2)]
+    d <- length(vmu)
     
-    vmu.til     <- mX%*%vbeta+mZ%*%vu
-    vsigma2.til <- diag(cbind(mX,mZ)%*%mLambda%*%t(cbind(mX,mZ)))
+    vmu.til     <- mX%*%vmu
+    vsigma2.til <- diag(mX%*%mLambda%*%t(mX))
     vB0 <- B0.fun("POISSON",vmu.til,vsigma2.til,gh) 
     
-    f <- sum(vr*vy*vmu.til - vB0) - 0.5*(t(vbeta)%*%mSigmaBeta.inv%*%vbeta + t(vu)%*%mSigma.inv%*%vu)
-    f <- f - 0.5*(d1+d2)*log(2*pi) + 0.5*(log(det(mSigmaBeta.inv)) + log(det(mSigma.inv)) )
+    f <- sum(vy*vmu.til - vB0) - 0.5*t(vmu)%*%mSigma.inv%*%vmu 
+    f <- f - 0.5*d*log(2*pi) + 0.5*log(det(mSigma.inv)) 
     return(f)
 }
 
 ###############################################################################
 
-f.GVA <- function(vmu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,gh,mR,Rinds,Dinds)
-# Calculate lower bound.
+f.GVA <- function(vtheta,vy,mX,mSigma.inv,gh,mR,Rinds,Dinds)
 {
-	# Unpack vbeta, vu and mR from vu
-	d1 <- ncol(mX)
-   d2 <- ncol(mZ)  
-	vbeta = vmu[1:d1]
-   vu <- vmu[(d1+1):d2]
-   mR[Rinds] <- vmu[(1+d1+d2):length(vmu)]
+   d <- ncol(mX)  
+   vmu <- vtheta[1:d]
+   mR[Rinds] <- vtheta[(1+d):length(vtheta)]
    mR[Dinds] <- exp(mR[Dinds]) 
-	# Threshold entries in the diagonal of mR at 10,000
     for (i in 1:length(Dinds)) {
         mR[Dinds[i]] <- min(c(1.0E5,mR[Dinds[i]]))
     }   
    mLambda <- mR%*%t(mR)   
    
-	# Calculate f
-   f <- sum(log(diag(mR))) + f.G.zero(vmu,mLambda,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,gh) 
-   f <- f + 0.5*(d1+d2)*log(2*pi) + 0.5*(d1+d2)
+   f <- sum(log(diag(mR))) + f.G(vmu,mLambda,vy,mX,mSigma.inv,gh) 
+   f <- f + 0.5*d*log(2*pi) + 0.5*d
    
    if (!is.finite(f)) {
        f <- -1.0E16
@@ -124,48 +102,47 @@ f.GVA <- function(vmu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,gh,mR,Rinds,Dinds)
 
 ###############################################################################
 
-vg.G <- function(vmu,mLambda,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,vB1) 
+vg.G <- function(vmu,mLambda,vy,mX,mSigma.inv,vB1) 
 {
-    vg <- t(cbind(mX,mZ))%*%(vr*vy - vB1) - mSigmaBeta.inv%*%vbeta - mSigma.inv%*%vu     
+    vg <- t(mX)%*%(vy - vB1) - mSigma.inv%*%vmu     
     return(vg)
 }
 
 ###############################################################################
 
-mH.G <- function(vmu,mLambda,vr,vy,mZ,mSigma.inv,vB2) 
+mH.G <- function(vmu,mLambda,vy,mX,mSigma.inv,vB2) 
 {
     vw <-  vB2; dim(vw) <- NULL
-    mH <- -t(cbind(mX,mZ)*vw)%*%cbind(mX, mZ) - rbind(mSigmaBeta.inv, mSigma.inv)
+    mH <- -t(mX*vw)%*%mX - mSigma.inv
     return(mH)    
 }
 
 ###############################################################################
 
-vg.GVA.approx <- function(vmu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,gh,mR,Rinds,Dinds)
+vg.GVA.approx <- function(vtheta,vy,mX,mSigma.inv,gh,mR,Rinds,Dinds)
 {
-    n <- length(vy); P <- length(vu); d <- ncol(mZ)
-		# Question: Why do we do this epsilon averaging trick?
+    n <- length(vy); P <- length(vtheta); d <- ncol(mX)
 		eps <- 1.0E-6
-		f <- f.GVA.zero(vmu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,gh,mR,Rinds,Dinds)
+		f <- f.GVA(vtheta,vy,mX,mSigma.inv,gh,mR,Rinds,Dinds)
 		vg.approx <- matrix(0,P,1)
 		for (i in 1:P) {
-		   vup <- vu 
-		   vup[i] <- vu[i] + eps
-		   fp <- f.GVA.zero(vup,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,gh,mR,Rinds,Dinds)
+		   vthetap <- vtheta 
+		   vthetap[i] <- vtheta[i] + eps
+		   fp <- f.GVA(vthetap,vy,mX,mSigma.inv,gh,mR,Rinds,Dinds)
 		   
-		   vum <- vu 
-		   vum[i] <- vu[i] - eps
-		   fm <- f.GVA.zero(vum,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,gh,mR,Rinds,Dinds)
+		   vthetam <- vtheta 
+		   vthetam[i] <- vtheta[i] - eps
+		   fm <- f.GVA(vthetam,vy,mX,mSigma.inv,gh,mR,Rinds,Dinds)
 		   
 		   vg.approx[i] <- (fp - fm)/(2*eps)
 		   
 		   #vthetapp <- vtheta 
 		   #vthetapp[i] <- vtheta[i] + 2*eps
-		   #fpp <- f.GVA(vthetapp,vy,mZ,mSigma.inv,gh,mR,Rinds,Dinds)
+		   #fpp <- f.GVA(vthetapp,vy,mX,mSigma.inv,gh,mR,Rinds,Dinds)
 		   
 		   #vthetamm <- vtheta 
 		   #vthetamm[i] <- vtheta[i] - 2*eps
-		   #fmm <- f.GVA(vthetamm,vy,mZ,mSigma.inv,gh,mR,Rinds,Dinds)  
+		   #fmm <- f.GVA(vthetamm,vy,mX,mSigma.inv,gh,mR,Rinds,Dinds)  
 		   
 		   #vg.approx[i] <- (-fpp + 8*fp - 8*fm + fmm)/(12*eps)
 		}
@@ -174,34 +151,28 @@ vg.GVA.approx <- function(vmu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,gh,mR,Rinds,
 
 ###############################################################################
 
-vg.GVA <- function(vmu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,gh,mR,Rinds,Dinds)
+vg.GVA <- function(vtheta,vy,mX,mSigma.inv,gh,mR,Rinds,Dinds)
 {
-	# Unpack vbeta, vu and mR from vmu
-	d1 <- ncol(mX)
-    d2 <- ncol(mZ)
-	vbeta <- vmu[1:d1]
-    vu <- vmu[(d1+1):(d1+d2)]
-    mR[Rinds] <- vmu[(1+d1+d2):length(vmu)]
-
+    d <- length(vmu)
+    vmu <- vtheta[1:d]
+    mR[Rinds] <- vtheta[(1+d):length(vtheta)]
     mR[Dinds] <- exp(mR[Dinds]) 
-	# Threshold entries of mR at 1,000
     for (i in 1:length(Dinds)) {
         mR[Dinds[i]] <- min(c(1.0E3,mR[Dinds[i]]))
     }    
     mLambda <- mR%*%t(mR)   
   
-    vmu.til     <- mZ%*%vmu
-    vsigma2.til <- diag(mZ%*%mLambda%*%t(mZ))
+    vmu.til     <- mX%*%vmu
+    vsigma2.til <- diag(mX%*%mLambda%*%t(mX))
     res.B12 <- B12.fun("POISSON",vmu.til,vsigma2.til,gh)
     vB1 <- res.B12$vB1
     vB2 <- res.B12$vB2
     
-	# Set the derivative of vmu in the first d entries of vg
-    vg <- 0*vu
-    vg[1:(d1+d2)] <- vg.G.zero(vmu,mLambda,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,vB1) 
+    vg <- 0*vtheta
+    vg[1:d] <- vg.G(vmu,mLambda,vy,mX,mSigma.inv,vB1) 
 
     mLambda.inv <- solve(mLambda,tol=1.0E-99)
-    mH <- mH.G.zero(vmu,mLambda,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,vB2)
+    mH <- mH.G(vmu,mLambda,vy,mX,mSigma.inv,vB2)
     dmLambda <- (mLambda.inv + mH)%*%mR
 
     #count <- d+1
@@ -216,11 +187,10 @@ vg.GVA <- function(vmu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,gh,mR,Rinds,Dinds)
     #    }      
     #}
     
-	# Set the derivative of mR in the last entries of vg
     dmLambda[Dinds] <- dmLambda[Dinds]*mR[Dinds]
-    vg[(1+d1+d2):length(vu)] <- dmLambda[Rinds]    
+    vg[(1+d):length(vtheta)] <- dmLambda[Rinds]    
    
-    #vg.approx <- vg.GVA.approx(vu,vy,mZ,mSigma.inv,gh,mR,Rinds,Dinds)
+    #vg.approx <- vg.GVA.approx(vtheta,vy,mX,mSigma.inv,gh,mR,Rinds,Dinds)
     #print(cbind(vg,vg.approx,abs(vg - vg.approx),abs(vg - vg.approx)/abs(vg),vg/vg.approx))
     #ans <- readline()            
               
@@ -229,23 +199,21 @@ vg.GVA <- function(vmu,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,gh,mR,Rinds,Dinds)
 
 ###############################################################################
 
-fit.GVA <- function(vbeta,vu,mLambda,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,method,reltol=1.0e-8)
+fit.GVA <- function(vmu,mLambda,vy,mX,mSigma.inv,method,reltol=1.0e-8)
 {
-	#library(statmod)
-	#N <- 15
-	#gh  <- gauss.quad(N,kind="hermite")
-	gh2 <- NULL #list(x=gh$nodes,w=gh$weights,w.til=gh$weights*exp(gh$nodes^2))    
-	vmu <- c(vbeta, vu)
+		#library(statmod)
+		#N <- 15
+		#gh  <- gauss.quad(N,kind="hermite")
+		gh2 <- NULL #list(x=gh$nodes,w=gh$weights,w.til=gh$weights*exp(gh$nodes^2))    
+		
     d <- length(vmu)
-    # Calculate the diagonal indices
     Dinds <- d*((1:d)-1)+(1:d) 		
           
     mR <- t(chol(mLambda + diag(1.0E-8,d)))
-    # Question: Why do we only take the log of the diagonal entries?
     mR[Dinds] <- log(mR[Dinds]) 
     Rinds <- which(lower.tri(mR,diag=TRUE))
-    vu <- c(vmu,mR[Rinds])
-    P <- length(vu)
+    vtheta <- c(vmu,mR[Rinds])
+    P <- length(vtheta)
     
     if (method=="L-BFGS-B") {
         controls <- list(maxit=1000,trace=0,fnscale=-1,REPORT=1,factr=1.0E-5,lmm=10)
@@ -254,17 +222,16 @@ fit.GVA <- function(vbeta,vu,mLambda,vr,vy,mX,mZ,mSigmaBeta.inv,mSigma.inv,metho
     } else {
         controls <- list(maxit=1000,trace=0,fnscale=-1,REPORT=1,reltol=reltol) 
     }
-    res <- optim(par=vu, fn=f.GVA.zero, gr=vg.GVA.zero,
+    res <- optim(par=vtheta, fn=f.GVA, gr=vg.GVA,
         method=method,lower=-Inf, upper=Inf, control=controls,
-        vr=vr,vy=vy,mX=mX,mZ=mZ,mSigmaBeta.inv=mSigmaBeta.inv,mSigma.inv=mSigma.inv,
-		gh=gh2,mR=mR*0,Rinds=Rinds,Dinds=Dinds)        
+        vy=vy,mX=mX,mSigma.inv=mSigma.inv,gh=gh2,mR=mR*0,Rinds=Rinds,Dinds=Dinds)        
         
-    vu <- res$par 
+    vtheta <- res$par 
     
-    #f <- f.GVA(vu,vy,mC,CTC,CTy,p,K,mR,Rinds,Dinds)
+    #f <- f.GVA(vtheta,vy,mC,CTC,CTy,p,K,mR,Rinds,Dinds)
     
-    vmu <- vu[1:d]
-    mR[Rinds] <- vu[(1+d):P]
+    vmu <- vtheta[1:d]
+    mR[Rinds] <- vtheta[(1+d):P]
     mR[Dinds] <- exp(mR[Dinds])  
     mLambda <- mR%*%t(mR)
        
