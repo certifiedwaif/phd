2970a998e4a69d2ae78815f1bebc9f75a7b14a01
Mark Greenaway <markg@maths.usyd.edu.au>
2014-07-07 13:52:53 +1000

    Add John's faster functions to common code, to eventually be used everywhere.

--- code/common.R
+++ code/common.R
@@ -2,3 +2,14 @@
 logit <- function(p) log(p/(1-p))
 expit <- function(eta) 1/(1+exp(-eta))
 tr <- function(mX) sum(diag(mX))
+
+my.diag <- function(mA) {
+    k <- ncol(mA) 
+    Dinds <- k*((1:k)-1) + (1:k)
+    val <- mA[Dinds]
+    return(val)
+}
+
+my.tr <- function(mA) {
+	return( sum(my.diag(mA)) ) 
+}
