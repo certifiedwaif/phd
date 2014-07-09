161d98f5493a79dfe5d22262f11892525f9f938f
Mark Greenaway <markg@maths.usyd.edu.au>
2014-07-09 15:46:58 +1000

    Initial check-in

---
+++ code/stan_example1.R
@@ -0,0 +1,30 @@
+require(rstan)
+schools_code <- '
+  data {
+    int<lower=0> J; // number of schools 
+    real y[J]; // estimated treatment effects
+    real<lower=0> sigma[J]; // s.e. of effect estimates 
+  }
+  parameters {
+    real mu; 
+    real<lower=0> tau;
+    real eta[J];
+  }
+  transformed parameters {
+    real theta[J];
+    for (j in 1:J)
+      theta[j] <- mu + tau * eta[j];
+  }
+  model {
+    eta ~ normal(0, 1);
+    y ~ normal(theta, sigma);
+  }
+'
+
+schools_dat <- list(J = 8, 
+                    y = c(28,  8, -3,  7, -1,  1, 18, 12),
+                    sigma = c(15, 10, 16, 11,  9, 11, 10, 18))
+
+fit <- stan(model_code = schools_code, data = schools_dat, 
+            iter = 1000, chains = 4)
+plot(fit)
