43105749c56906a385b5b6d1dbe0f0c0296965fc
Mark Greenaway <certifiedwaif@gmail.com>
2014-12-17 12:14:47 +1100

    Move fastdiag C++ code to its own source file.

---
+++ code/fastdiag.cpp
@@ -0,0 +1,26 @@
+#include <RcppEigen.h>
+using namespace Rcpp;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+// [[Rcpp::depends(RcppEigen)]]
+
+// For more on using Rcpp click the Help button on the editor toolbar
+
+typedef Eigen::Map<Eigen::MatrixXd> MapMatd;
+
+// [[Rcpp::export]]
+VectorXd fastdiag(MapMatd C, MapMatd lambda)
+{
+  VectorXd result(C.rows());
+
+  if (C.cols() != lambda.cols()) {
+    stop("mC and mLambda do not have the same numbers of columns");
+  }  
+
+  for (int i = 0; i < C.rows(); i++) {
+    result[i] = C.row(i) * lambda * C.row(i).transpose();
+  }
+
+  return result;
+}
