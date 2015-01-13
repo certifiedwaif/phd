f47d68c64af3f09ec972b6ae26c7debb66e087a4
Mark Greenaway <certifiedwaif@gmail.com>
2015-01-13 21:46:21 +1100

    Some bug fixes. Use SimplicialCholesky solver. Inverse is a bit wrong, which is vexing.

--- code/fastdiag.cpp
+++ code/fastdiag.cpp
@@ -1,5 +1,6 @@
 // [[Rcpp::depends(RcppEigen)]]
 #include <RcppEigen.h>
+#include <Eigen/SparseCholesky>
 #include <Eigen/IterativeLinearSolvers>
 
 using std::vector;
@@ -7,11 +8,11 @@ using namespace Rcpp;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using Eigen::SparseMatrix;
+using Eigen::SimplicialCholesky;
 using Eigen::ConjugateGradient;
 
 typedef Eigen::Map<Eigen::MatrixXd> MapMatd;
 typedef Eigen::SparseMatrix<double> SpMat;
-
 // [[Rcpp::export]]
 VectorXd fastdiag(MapMatd C, MapMatd lambda)
 {
@@ -36,6 +37,8 @@ SpMat fastinv(const MapMatd Rd, const int p, const int m, const int blocksize, c
   
   typedef Eigen::Triplet<double> T;
   std:vector<T> triplets;
+  
+  //std::cout << "Built triplets list" << std::endl;
   triplets.reserve(m*blocksize/2 + p*p/2 + p*m*blocksize);
   // Insert entries for random effects
   for (int m_idx = 0; m_idx < m; m_idx++) {
@@ -44,31 +47,49 @@ SpMat fastinv(const MapMatd Rd, const int p, const int m, const int blocksize, c
     // b for the bth row
     for (int b_idx = 0; b_idx < blocksize; b_idx++) {
       const int offset = m_idx*blocksize;
-      for (int row_idx = offset; row_idx < offset+b_idx; row_idx++) {
+      //std::cout << "offset " << offset << std::endl;
+      for (int row_idx = offset; row_idx < offset+b_idx+1; row_idx++) {
+        //std::cout << "row_idx" << row_idx << std::endl;
         int i = offset+b_idx;
         int j = offset+row_idx;
+        //std::cout << "loop: i " << i << " j " << j << " Rd(i, j) " << Rd(i, j) << std::endl;
+
         triplets.push_back(T(i, j, Rd(i, j)));
       }
     }
   }
   
+  //std::cout << "loop2" << std::endl;
   // Insert entries for fixed effects
   for (int p_idx = m*blocksize; p_idx < m*blocksize+p; p_idx++) {
-    for (int row_idx = 0; row_idx < p_idx; row_idx++) {
+    //std::cout << "p_idx " << p_idx << std::endl;
+    for (int row_idx = 0; row_idx < p + m*blocksize; row_idx++) {
       int i = p_idx;
       int j = row_idx;
-      triplets.push_back(T(i, j, Rd(i, j)));
+      
+      //std::cout << "row_idx " << row_idx << std::endl;
+      
+      //std::cout << "loop2: i " << i << " j " << j << " Rd(i, j) " << Rd(i, j) << std::endl;
+
+    triplets.push_back(T(i, j, Rd(i, j)));
     }
   }
-
-  SpMat Rsp;
+  
+  //std::cout << "Constructing sparse matrix" << std::endl;
+  SpMat Rsp(Rd.rows(), Rd.cols());
   Rsp.setFromTriplets(triplets.begin(), triplets.end());
   
+  //std::cout << Rsp << std::endl;
+  
   // Solve for RHS = I
-  ConjugateGradient<SparseMatrix<double> > solver;
+  //std::cout << "Solving" << std::endl;
+  Eigen::SimplicialCholesky<SpMat> solver;
+  //std::cout << "maxIterations" << solver.maxIterations() << std::endl;
+  //solver.setTolerance(1e-99);
   solver.compute(Rsp);
   SparseMatrix<double> I(Rd.rows(),Rd.cols());
   I.setIdentity();
-  
+  //std::cout << "I " << I << std::endl;
+  //std::cout << "Returning" << std::endl;
   return solver.solve(I);
 }
