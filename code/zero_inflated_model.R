41092bc354dd8373fa02e1a80bc17a8dea5cc819
Mark Greenaway <certifiedwaif@gmail.com>
2015-02-25 15:52:30 +1100

    Move univariate routines to their own file. Change variational approximation routine to match new data generation.

--- code/zero_inflated_model.R
+++ code/zero_inflated_model.R
@@ -3,38 +3,6 @@ library(limma)
 source("common.R")
 source("gaussian.R")
 
-mcmc.univariate <- function(iterations, vx, a, b)
-{
-  # Initialise
-  n = length(vx)
-  zero.set = which(vx == 0)
-  nonzero.set = which(vx != 0)
-  vlambda = rep(NA, iterations+1)
-  vrho = rep(NA, iterations+1)
-  vlambda[1] = mean(vx)
-  vrho[1] = length(zero.set)/length(vx)
-  veta = rep(NA, n)
-  vr = rep(NA, n)
-  # Build up a list of the zero observations. We'll only generate
-  # r[i]'s for those. The non-zero observations will always have r[i] = 1.
-  vr[nonzero.set] = 1
-  # Iterate ----
-  for (i in 1:iterations) {
-    varg = exp(-vlambda[i] + logit(vrho[i]))
-    veta = varg/(1 + varg)
-    
-    # The following code is correct, but 100 times slower as R is interpreted.
-    # So we use the vectorised code instead.
-    #for (j in 1:length(zero_observation_idx)) {
-    #	r[zero_observation_idx[j]] = rbinom(1, 1, eta)
-    #}
-    
-    vr[zero.set] = rbinom(length(zero.set), 1, veta) 
-    vlambda[i+1] = rgamma(1, a + sum(vx), b + sum(vr))
-    vrho[i+1] = rbeta(1, sum(vr) + 1, n - sum(vr) + 1)
-  }
-  return(list(vlambda=vlambda, vrho=vrho))
-}
 
 mcmc <- function(object)
 {
@@ -50,39 +18,6 @@ beta_entropy <- function(alpha, beta)
 {
   lbeta(alpha, beta) - (alpha-1)*digamma(alpha) - (beta-1)*digamma(beta) + (alpha+beta-2)*digamma(alpha+beta)
 }	
-
-calculate_lower_bound.univariate <- function(univariate)
-{
-  vx = univariate$vx
-  vp = univariate$vp
-  a_lambda = univariate$a_lambda
-  b_lambda = univariate$b_lambda
-  a_rho = univariate$a_rho
-  b_rho = univariate$b_rho
-  
-  zero.set <- which(vx==0)
-  
-  E_lambda = a_lambda/b_lambda
-  E_log_lambda = digamma(a_lambda) - log(b_lambda)	
-  
-  E_r = ifelse(vx == 0, vp, 1)
-  E_xi_log_lambda_r = ifelse(vx == 0, 0, vx*E_log_lambda)
-  
-  E_log_rho = digamma(a_rho) - digamma(a_rho + b_rho)
-  E_log_one_minus_rho = digamma(b_rho) - digamma(a_rho + b_rho)
-  
-  E_log_q_r = (vp[zero.set]*log(vp[zero.set]) + (1-vp[zero.set])*log(1-vp[zero.set]))
-  E_log_q_lambda = -gamma_entropy(a_lambda, b_lambda)
-  E_log_q_rho = -beta_entropy(a_rho, b_rho) # FIXME: Why is this entropy positive?
-  
-  result = a_lambda * log(b_lambda) + (a_lambda-1) * E_log_lambda - b_lambda * E_lambda - lgamma(a_lambda)
-  result = result - E_lambda * sum(E_r)
-  result = result + sum(E_xi_log_lambda_r) - sum(lgamma(vx+1))
-  result = result + sum(E_r) * E_log_rho + sum(1 - E_r) * E_log_one_minus_rho
-  result = result - sum(E_log_q_r) - E_log_q_lambda - E_log_q_rho
-  
-  return(result)
-}
 
 calculate_lower_bound.multivariate <- function(multivariate, verbose=FALSE)
 {
@@ -122,11 +57,11 @@ calculate_lower_bound.multivariate <- function(multivariate, verbose=FALSE)
       .25*p*(p-1)*log(pi) + sum(lgamma(a + .5*(1 - 1:p)))
     }
     
-    E_log_det_mSigma_u = sum(digamma(.5*(v + 1 - 1:p))) + p*log(2) - log(det(psi))
-    E_mSigma_u_inv = solve(psi)/(v+p+1)
-    T2 = .5*(prior$v*log(det(prior$psi)) - v*log(det(psi)))
+    E_log_det_mSigma_u = sum(digamma(.5*(v + 1 - 1:p))) + p*log(2) - log(det(mPsi))
+    E_mSigma_u_inv = solve(mPsi)/(v+p+1)
+    T2 = .5*(prior$v*log(det(prior$mPsi)) - v*log(det(mPsi)))
     T2 = T2 - lgammap(.5*prior$v, p) + lgammap(.5*v, p)
-    T2 = T2 + .5*E_log_det_mSigma_u - .5*tr(E_mSigma_u_inv*(prior$psi - psi))
+    T2 = T2 + .5*E_log_det_mSigma_u - .5*tr(E_mSigma_u_inv*(prior$mPsi - mPsi))
     
     # Something is wrong in T3. It sometimes goes backwards as we're optimised.
     # This should be unchanged from the univariate lower bound
@@ -151,59 +86,8 @@ calculate_lower_bound <- function(object, verbose=verbose)
   UseMethod("calculate_lower_bound", object)
 }
 
-create_univariate <- function(vx, a, b)
-{
-  # Initialise
-  n = length(vx)
-  vp = rep(1, n)
-  a_lambda = a + sum(vx)
-  b_lambda = b
-  univariate = list(vx=vx, vp=vp, a_lambda=a_lambda, b_lambda=b_lambda)
-  class(univariate) = "univariate"
-  return(univariate)
-}
 
-zero_infl_var.univariate <- function(univariate, verbose=FALSE, plot_lower_bound=FALSE)
-{
-  vx = univariate$vx
-  a = univariate$a_lambda
-  b = univariate$b_lambda
-  n = length(univariate$vx)
-  zero.set = which(univariate$vx == 0)
-  nonzero.set = which(univariate$vx != 0)
-  vlower_bound <- c()
-  
-  i = 0
-  # Iterate ----
-  while (i <= 2 || vlower_bound[i] > vlower_bound[i-1]) {
-    i = i+1
-    
-    # Update parameter for q_lambda
-    univariate$b_lambda = b + sum(univariate$vp)
-    
-    # Update parameters for q_rho
-    univariate$a_rho = 1 + sum(univariate$vp)
-    univariate$b_rho = n - sum(univariate$vp) + 1
-    
-    # Update parameters for q_vr
-    univariate$vp[zero.set] = expit(-expected_lambda(univariate) + digamma(univariate$a_rho) - digamma(univariate$b_rho))
-    
-    vlower_bound[i] <- calculate_lower_bound(univariate)
-    
-    if (verbose && i > 1)
-      cat("Iteration ", i, ": lower bound ", vlower_bound[i], " difference ",
-          vlower_bound[i] - vlower_bound[i-1], " parameters ", "a_lambda", univariate$a_lambda,
-          "b_lambda", univariate$b_lambda, "a_rho", univariate$a_rho, "b_rho", univariate$b_rho, "\n")
-  }
-  
-  if (plot_lower_bound)
-    plot(lower_bound_vector,type="l")
-  
-  params = list(a_lambda=univariate$a_lambda, b_lambda=univariate$b_lambda, a_rho=univariate$a_rho, b_rho=univariate$b_rho)
-  return(params)
-}
-
-create_multivariate <- function(vy, mX, mZ, sigma2.beta, a_sigma, b_sigma, tau, m=ncol(mZ), blocksize=1, spline_dim=NA, v=m+2)
+create_multivariate <- function(vy, mX, mZ, sigma2.beta, a_sigma, b_sigma, tau, m=ncol(mZ), blocksize=1, spline_dim=NA, v=blocksize+1)
 {
   # Initialise
   n = length(vy)
@@ -227,10 +111,11 @@ create_multivariate <- function(vy, mX, mZ, sigma2.beta, a_sigma, b_sigma, tau, 
   b_rho = n - sum(vp) + 1
   
   # Set prior parameters for Inverse Wishart distribution
-  psi=diag(1, rep(blocksize))
+  #mPsi=diag(1, rep(blocksize))
+  mPsi = 1e-5*matrix(c(1, 0, 0, 1), 2, 2)
   
   prior = list(a_sigma=a_sigma, b_sigma=b_sigma,
-               v=v, psi=psi,
+               v=v, mPsi=mPsi,
                a_rho=1, b_rho=1,
                sigma2.beta=sigma2.beta)
   v=prior$v + m
@@ -238,7 +123,7 @@ create_multivariate <- function(vy, mX, mZ, sigma2.beta, a_sigma, b_sigma, tau, 
                       mX=mX, mZ=mZ, mC=mC,
                       m=m, blocksize=blocksize, spline_dim=spline_dim,
                       a_sigma=a_sigma, b_sigma=b_sigma,
-                      p=p, v=v, psi=psi,
+                      p=p, v=v, mPsi=mPsi,
                       a_rho=a_rho, b_rho=b_rho,
                       mLambda=mLambda,
                       prior=prior,
@@ -251,7 +136,7 @@ create_multivariate <- function(vy, mX, mZ, sigma2.beta, a_sigma, b_sigma, tau, 
 
 zero_infl_var.multivariate <- function(mult, method="gva", verbose=FALSE, plot_lower_bound=FALSE)
 {
-  MAXITER <- 1000
+  MAXITER <- 30
   
   # Initialise
   N = length(mult$vy)
@@ -326,7 +211,7 @@ zero_infl_var.multivariate <- function(mult, method="gva", verbose=FALSE, plot_l
     
     # Update parameters for q_vr
     if (length(zero.set) != 0) {
-      mult$vp[zero.set] = expit((mult$vy[zero.set]*mult$mC[zero.set,])%*%mult$vmu-exp(mult$mC[zero.set,]%*%mult$vmu + 0.5*diag((matrix(mult$mC[zero.set,], length(zero.set), p+m*blocksize+spline_dim))%*%mult$mLambda%*%t(matrix(mult$mC[zero.set,], length(zero.set), p+m*blocksize+spline_dim))) + digamma(mult$a_rho) - digamma(mult$b_rho)))
+      mult$vp[zero.set] = expit((mult$vy[zero.set]*mult$mC[zero.set,])%*%mult$vmu-exp(mult$mC[zero.set,]%*%mult$vmu + 0.5*diag((matrix(mult$mC[zero.set,], length(zero.set), p+(m-1)*blocksize+spline_dim))%*%mult$mLambda%*%t(matrix(mult$mC[zero.set,], length(zero.set), p+(m-1)*blocksize+spline_dim))) + digamma(mult$a_rho) - digamma(mult$b_rho)))
     }
     
     # Update parameters for q_rho
@@ -336,7 +221,7 @@ zero_infl_var.multivariate <- function(mult, method="gva", verbose=FALSE, plot_l
     # Update parameters for q_sigma_u^2 if we need to
     if (!is.null(mult$mZ)) {
       # a_sigma is fixed
-      u_dim = m*blocksize+spline_dim
+      u_dim = (m-1)*blocksize+spline_dim
       mult$a_sigma = mult$prior$a_sigma + u_dim/2
       u_idx = p + 1:u_dim  # (ncol(mult$mX)+1):ncol(mult$mC)
       # u_idx = p + 1:(m*blocksize) + spline_dim
@@ -344,20 +229,22 @@ zero_infl_var.multivariate <- function(mult, method="gva", verbose=FALSE, plot_l
       #mult$b_sigma = mult$prior$b_sigma + sum(vu^2)/2 + (tr_mSigma)/2
       mult$b_sigma = mult$prior$b_sigma + sum(mult$vmu[u_idx]^2)/2 + tr(mult$mLambda[u_idx, u_idx])/2    # Extract right elements of mLambda
       #browser()
+      
+      # FIXME: This code is hard to read
       acc = matrix(0, blocksize, blocksize)
-      for (j in 1:m) {
+      for (j in 1:(m-1)) {
         j_idx = p + (j-1)*blocksize+(1:blocksize)
         acc = acc + with(mult, vmu[j_idx] %*% t(vmu[j_idx]) + mLambda[j_idx, j_idx])
       }
-      mult$psi = with(mult, prior$psi + acc)
+      mult$mPsi = with(mult, prior$mPsi + acc)
       
-      tau_sigma = mult$a_sigma/mult$b_sigma
+      #tau_sigma = mult$a_sigma/mult$b_sigma
       
-      mult$mSigma.u.inv = diag(tau_sigma, u_dim)
+      #mult$mSigma.u.inv = diag(tau_sigma, u_dim)
   
-      mult$mSigma.u.inv = with(mult, kronecker(diag(1, m), solve(psi/(v - blocksize - 1))))
+      mult$mSigma.u.inv = with(mult, kronecker(diag(1, m-1), solve(mPsi/(v - blocksize - 1))))
       
-      #mult$mSigma.u.inv = with(mult, solve(psi)) # What multiplicative factor for psi?
+      #mult$mSigma.u.inv = with(mult, solve(mPsi)) # What multiplicative factor for psi?
     }
     
     vlower_bound[i] <- 0 # calculate_lower_bound(mult)
