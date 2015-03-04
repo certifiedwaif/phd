0cdb99454226f1f01d00ddfd8c852e040327bfea
Mark Greenaway <markg@maths.usyd.edu.au>
2015-03-04 15:10:26 +1100

    Remove cruft. Use new version of Stan function which runs in parallel.

--- code/mcmc.R
+++ code/mcmc.R
@@ -1,29 +1,21 @@
 # mcmc.R
-mcmc_approximation <- function(mult, iterations=1e3, warmup=floor(iterations/2), mc.cores = 1)
+require(rstan)
+require(parallel)
+# Andrew Gelman says that this magical line of code automatically makes Stan
+# run in parallel and cache compiled models.
+# Two minutes later: Hey, it actually works!
+source("http://mc-stan.org/rstan/stan.R")
+
+mcmc_approximation <- function(mult, seed=1, iterations=1e3, warmup=floor(iterations/2), mc.cores = 1)
 {
-  #mcmc_result = mcmc(mult, iterations=1e5+2000, burnin=2000, thinning=1)
   # Use Stan to create MCMC samples, because Stan deals much better with highly
   # correlated posteriors.
-  require(rstan)
-  require(parallel)
-  #source("multivariate_stan.R")
   
-  #mS = matrix(c(1, 0, 0, 1), 2, 2)
   u_dim = with(mult, m*blocksize+spline_dim)
   zip_data <- with(mult, list(N=length(vy), P=2, M=m, B=blocksize, y=vy, X=mX, Z=mZ,
                               psi=prior$mPsi, BetaPrior=mSigma.beta)) #, v=prior$v))
-  #print(str(zip_data))
-  rng_seed <- 5;
-  fit <- stan("multivariate_zip.stan", data=zip_data, iter=iterations, warmup=warmup, chains = 1)
-  #sflist <- 
-  #  mclapply(1:4, mc.cores = mc.cores, 
-  #           function(i) stan(fit=foo, data=zip_data, seed = rng_seed, 
-  #                            chains = 1, chain_id = i, refresh = -1,
-  #                            iter=iterations))
-  #fit <- sflist2stanfit(sflist)
-  
-  #fit <- stan(model_code = zip_code, data = zip_dat, 
-  #            iter = 1e5, chains = 4)  
+  fit <- stan("multivariate_zip.stan", seed=seed, data=zip_data, iter=iterations/32, warmup=warmup/32, chains = 32)
   mcmc_samples = extract(fit)
+
   return(mcmc_samples)
 }
