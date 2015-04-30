bfa28491158fe472c8b255bd84bf2a0a18dc3ea3
Mark Greenaway <markg@maths.usyd.edu.au>
2015-04-30 18:00:39 +1000

    Pass v through to Stan.

--- code/mcmc.R
+++ code/mcmc.R
@@ -1,9 +1,9 @@
 # mcmc.R
 library(rstan)
-library(parallel)
 # Andrew Gelman says that this magical line of code automatically makes Stan
 # run in parallel and cache compiled models.
 # Two minutes later: Hey, it actually works!
+# Some time later: Sometimes it works.
 #source("http://mc-stan.org/rstan/stan.R")
 
 mcmc_approximation <- function(mult, seed=1, iterations=NA, warmup=NA, mc.cores=1,
@@ -19,11 +19,12 @@ mcmc_approximation <- function(mult, seed=1, iterations=NA, warmup=NA, mc.cores=
   mZ <- mult$mZ
   vy <- mult$vy
   mPsi <- mult$prior$mPsi
+  v <- mult$prior$v
   mSigma.beta <- mult$mSigma.beta
   
   zip_data <- list(N=length(vy), P=2, M=m, B=blocksize, spline_dim=spline_dim,
   								 y=vy, X=mX, Z=mZ,
-                   psi=mPsi, BetaPrior=mSigma.beta) #, v=prior$v))
+                   v=v, psi=mPsi, BetaPrior=mSigma.beta)
 
 	fit <- stan(stan_file, seed=seed, data=zip_data, iter=iterations, warmup=warmup, chains=1)
   mcmc_samples <- extract(fit)
