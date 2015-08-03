1652e9b6cd26ef96e5ed865acfffb25db3296153
Mark Greenaway <markg@maths.usyd.edu.au>
2015-08-03 17:19:37 +1000

    Updates.

--- code/application.R
+++ code/application.R
@@ -11,16 +11,20 @@ Ids <- sqldf("select Id, count(*) from long12_analyse group by Id having count(*
 long12_subset <- subset(long12_analyse, Id %in% Ids$Id)
 # Construct vy, mX and mZ matrices.
 vy <- long12_subset$mintot
-mX <- subset(long12_subset, select=-mintot)
-n <- dim(Ids)[1]
-m <- 3
-mZ <- matrix(0, m * n, n * 2)
-i <- 1:(m * n)
-j <- rep(1:n, each=m)
+mX <- subset(long12_subset, select=-c(mintot, Id))
+m <- dim(Ids)[1]
+ni <- 3
+mZ <- matrix(0, ni * m, m * 2)
+i <- 1:(ni * m)
+j <- rep(1:m, each=ni)
 for (idx in 1:length(i)) {
 	mZ[i[idx], (j[idx] - 1) * 2 + 1] <- 1
 	mZ[i[idx], (j[idx] - 1) * 2 + 2] <- mX$time[idx]
 }
+mX2 <- model.matrix(~., data=mX)
+# Construct mult object
+mult <- create_mult(vy, mX2, mZ, 1e-5, m=125, blocksize=2)
 # VB fit.
+fit <- zipvb(mult)
 # MCMC fit.
 # Compare.
