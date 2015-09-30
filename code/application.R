047613293da24f0f95f5ea889685c03a5178a51b
Mark Greenaway <markg@maths.usyd.edu.au>
2015-09-30 18:22:44 +1000

    Comment out old application, which we aren't doing any more.

--- code/application.R
+++ code/application.R
@@ -1,57 +1,57 @@
 # application.R
 
-library(sqldf)
-source("zero_inflated_model.R")
-source("accuracy.R")
+# library(sqldf)
+# source("zero_inflated_model.R")
+# source("accuracy.R")
 
-# Load the data set.
-long12_analyse <- read.csv(file="data/long12_analyse.csv", header=TRUE)
-long12_analyse <- long12_analyse[order(long12_analyse$Id, long12_analyse$time), ]
-# Drop individuals that we don't observe at all three time points.
-Ids <- sqldf("select Id, count(*) from long12_analyse group by Id having count(*) = 3")
-long12_subset <- subset(long12_analyse, Id %in% Ids$Id)
-# Construct vy, mX and mZ matrices.
-vy <- long12_subset$mintot
-mX <- subset(long12_subset, select=-c(mintot, Id))
-mX <- cbind(1, subset(long12_subset, select=time))
-# Scale time, otherwise everything overlows and fails horribly
-mX$time <- scale(mX$time)
-m <- dim(Ids)[1]
-ni <- 3
-mZ <- matrix(0, ni * m, m * 2)
-i <- 1:(ni * m)
-j <- rep(1:m, each=ni)
-for (idx in 1:length(i)) {
-	mZ[i[idx], (j[idx] - 1) * 2 + 1] <- 1
-	mZ[i[idx], (j[idx] - 1) * 2 + 2] <- mX$time[idx]
-}
-# threshold <- 300
-# vy[vy > threshold] <- threshold
-# vy <- vy / 10C
-# Construct mult object
-# mult <- create_mult(vy, mX, mZ[, 3:ncol(mZ)], 1e-5, m=125, blocksize=2)
-# There were two zeroes in a row, so I interpolated.
-vy[194] <- 30
-mult <- create_mult(vy, as.matrix(mX), mZ[, 3:ncol(mZ)], 1e5,
-										m=125, blocksize=2)
-# Initialise vmu to something not too far off, but without giving the whole game away.
-mult$vmu[1:2] <- c(log(1 + mean(vy)), 1)
-#mult$mLambda <- solve(crossprod(mult$mC))
-# mult$vmu <- c(coef(fit))
-# VB fit.
-# TODO: This overflows! Fix. Look at the likelihood, and see where it's
-# coming from.
+# # Load the data set.
+# long12_analyse <- read.csv(file="data/long12_analyse.csv", header=TRUE)
+# long12_analyse <- long12_analyse[order(long12_analyse$Id, long12_analyse$time), ]
+# # Drop individuals that we don't observe at all three time points.
+# Ids <- sqldf("select Id, count(*) from long12_analyse group by Id having count(*) = 3")
+# long12_subset <- subset(long12_analyse, Id %in% Ids$Id)
+# # Construct vy, mX and mZ matrices.
+# vy <- long12_subset$mintot
+# mX <- subset(long12_subset, select=-c(mintot, Id))
+# mX <- cbind(1, subset(long12_subset, select=time))
+# # Scale time, otherwise everything overlows and fails horribly
+# mX$time <- scale(mX$time)
+# m <- dim(Ids)[1]
+# ni <- 3
+# mZ <- matrix(0, ni * m, m * 2)
+# i <- 1:(ni * m)
+# j <- rep(1:m, each=ni)
+# for (idx in 1:length(i)) {
+# 	mZ[i[idx], (j[idx] - 1) * 2 + 1] <- 1
+# 	mZ[i[idx], (j[idx] - 1) * 2 + 2] <- mX$time[idx]
+# }
+# # threshold <- 300
+# # vy[vy > threshold] <- threshold
+# # vy <- vy / 10C
+# # Construct mult object
+# # mult <- create_mult(vy, mX, mZ[, 3:ncol(mZ)], 1e-5, m=125, blocksize=2)
+# # There were two zeroes in a row, so I interpolated.
+# vy[194] <- 30
+# mult <- create_mult(vy, as.matrix(mX), mZ[, 3:ncol(mZ)], 1e5,
+# 										m=125, blocksize=2)
+# # Initialise vmu to something not too far off, but without giving the whole game away.
+# mult$vmu[1:2] <- c(log(1 + mean(vy)), 1)
+# #mult$mLambda <- solve(crossprod(mult$mC))
+# # mult$vmu <- c(coef(fit))
+# # VB fit.
+# # TODO: This overflows! Fix. Look at the likelihood, and see where it's
+# # coming from.
 
-# Laplace works because there's no 0.5 * sigma2 argument to the exponential
-now <- Sys.time()
-fit <- zipvb(mult, method="gva", verbose=FALSE)
-print(Sys.time() - now)
+# # Laplace works because there's no 0.5 * sigma2 argument to the exponential
+# now <- Sys.time()
+# fit <- zipvb(mult, method="gva", verbose=FALSE)
+# print(Sys.time() - now)
 
-now <- Sys.time()
-fit2 <- zipvb(mult, method="gva2", verbose=FALSE)
-print(Sys.time() - now)
-# MCMC fit.
-# Compare.
+# now <- Sys.time()
+# fit2 <- zipvb(mult, method="gva2", verbose=FALSE)
+# print(Sys.time() - now)
+# # MCMC fit.
+# # Compare.
 
 # I might not be able to use that data set. Instead, I could use the roach data set
 # from ARM.
@@ -115,5 +115,5 @@ fit1 <- zipvb(mult, method="gva2", verbose=TRUE)
 
 # Check accuracy
 stan_fit <- mcmc(mult, p=3, iterations=1e5, warmup=1e4, mc.cores = 1)
-var_accuracy <- calculate_accuracies("application", mult, stan_fit$mcmc_samples, fit1, "gva2", plot_flag=TRUE)
-var_accuracy
+var_accuracy <- calculate_accuracies("application", mult, stan_fit$mcmc_samples, fit1, "gva_nr", plot_flag=TRUE)
+print(var_accuracy)
