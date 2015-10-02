f2329892b8531836a0f2af28ee1551bc199fee67
Mark Greenaway <markg@maths.usyd.edu.au>
2015-10-02 12:08:36 +1000

    Change problem size to be m=10, as this causes more stability problems for the various fitting algorithms.

--- code/local_solutions.R
+++ code/local_solutions.R
@@ -6,32 +6,47 @@ source("zero_inflated_model.R")
 local_solutions <- function(approximation)
 {
 	# Construct suitable mX, mZ and vy
-	vx <- rnorm(200)
+	n <- 100
+	vx <- rnorm(n)
 	mX <- cbind(1, vx)
 
 	# Construct mZ with three blocks
-	mZ <- cbind(rep(c(1, 0, 0), each=c(75, 75, 50)),
-							rep(c(0, 1, 0), each=c(75, 75, 50)),
-							rep(c(0, 0, 1), each=c(75, 75, 50)))
-	mZ <- mZ[1:200, 2:3]
-	vy <- rep(NA, 200)
-	true_intercept <- 3
+	mZ <- cbind(
+		rep(c(1, 0, 0, 0, 0, 0, 0, 0, 0, 0), each=rep(10, 10)),
+		rep(c(0, 1, 0, 0, 0, 0, 0, 0, 0, 0), each=rep(10, 10)),
+		rep(c(0, 0, 1, 0, 0, 0, 0, 0, 0, 0), each=rep(10, 10)),
+		rep(c(0, 0, 0, 1, 0, 0, 0, 0, 0, 0), each=rep(10, 10)),
+		rep(c(0, 0, 0, 0, 1, 0, 0, 0, 0, 0), each=rep(10, 10)),
+		rep(c(0, 0, 0, 0, 0, 1, 0, 0, 0, 0), each=rep(10, 10)),
+		rep(c(0, 0, 0, 0, 0, 0, 1, 0, 0, 0), each=rep(10, 10)),
+		rep(c(0, 0, 0, 0, 0, 0, 0, 1, 0, 0), each=rep(10, 10)),
+		rep(c(0, 0, 0, 0, 0, 0, 0, 0, 1, 0), each=rep(10, 10)),
+		rep(c(0, 0, 0, 0, 0, 0, 0, 0, 0, 1), each=rep(10, 10)),
+		rep(c(0, 0, 0, 0, 0, 0, 0, 0, 0, 1), each=rep(10, 10))
+	)
+	mZ <- mZ[1:n, 2:10]
+	vy <- rep(NA, n)
+	true_intercept <- 2
 	true_beta <- 1.0
-	for (i in 1:200) {
-		vy[i] <- rpois(1, lambda = true_intercept * mX[i, 1] + true_beta * mX[i, 2] + true_intercept * mZ[i, 1] + true_intercept * mZ[i, 2])
+	for (i in 1:n) {
+		lambda_param <- true_intercept * mX[i, 1] + true_beta * mX[i, 2]
+		lambda_param <- lambda_param + sum(true_intercept * mZ[i, 1:9])
+		vy[i] <- rpois(1, lambda = lambda_param)
 	}
 
 	# Construct mult object
 	sigma2.beta <- 1e5
-	mult <- create_mult(vy, mX, mZ, sigma2.beta, m=3, blocksize=1, v=2)
+	mult <- create_mult(vy, mX, mZ, sigma2.beta, m=10, blocksize=1, v=2)
 
 	counter <- 0
 	errors <- 0
-	vmus <- matrix(NA, 1e5, 4)
+	vmus <- matrix(NA, (10 * 1e2)^2, 2 + 9)
 
 	# Start fits from a range of points on a 2 dimensional grid
-	for (init_intercept in seq(-5, 5, 1e-1)) {
-		for (init_slope in seq(-5, 5, 1e-1)) {
+	GRID_BOUNDS <- 5
+	GRID_SIZE <- 1e-1
+	for (init_intercept in seq(- 4.5, GRID_BOUNDS, GRID_SIZE)) {
+		for (init_slope in seq(- 4.5, GRID_BOUNDS, GRID_SIZE)) {
 			cat("init_intercept", init_intercept, "\n")
 			cat("init_slope", init_slope, "\n")
 			cat("counter", counter, "\n")
@@ -39,12 +54,15 @@ local_solutions <- function(approximation)
 			mult$vmu[1] <- init_intercept
 			mult$vmu[2] <- init_slope
 			fit <- tryCatch(zipvb(mult, method=approximation, verbose=FALSE, glm_init=FALSE),
-											error = function(e) NULL)
+											error = function(e) {
+												print(e)
+												return(NULL)
+											})
 			if (is.null(fit)) {
 				print("Caught error")
 				errors <- errors + 1
 			} else {
-				print(str(fit))
+				# print(str(fit))
 				vmus[counter, ] <- fit$vmu
 			}
 			counter <- counter + 1
@@ -64,7 +82,7 @@ local_solutions <- function(approximation)
 	
 	cat("Counter", counter, "\n")
 	cat("Errors", errors, "\n")
-	print(vmus[1:(counter - 1), ])
+	return(vmus[1:(counter - 1), ])
 }
 
 main <- function()
@@ -72,6 +90,7 @@ main <- function()
   option_list <- list(make_option(c("-a", "--approximation"), default="gva"))
   opt <- parse_args(OptionParser(option_list=option_list))
   approximation <- opt$approximation
-  local_solutions(approximation)
+  vmus <- local_solutions(approximation)
+  write.csv(vmus, file=sprintf("local_solutions_%s_vmus.csv", approximation))
 }
 main()
