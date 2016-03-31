ed322f108c99070a7e19adc5a71b9d1d8a3d2bc0
Mark Greenaway <markg@maths.usyd.edu.au>
2016-03-31 22:33:21 +1100

    Include test cases. Define showBool() function.

--- code/canFormNote.cpp
+++ code/canFormNote.cpp
@@ -1,19 +1,21 @@
 // canFormNote.cpp
 #include <iostream>
 #include <string>
+#include <vector>
+#include <utility>
 
 using namespace std;
 
-bool canFormNote(string poster, string note)
+const bool canFormNote(const string& poster, const string& note)
 {
 	int letter_counts[26] = {};
 	
 	for (char c : poster) {
-		letter_counts[c - 'A']++;
+		letter_counts[c - 'A'] += 1;
 	}
 
 	for (char c : note) {
-		letter_counts[c - 'A']--;
+		letter_counts[c - 'A'] -= 1;
 		if (letter_counts[c - 'A'] < 0)
 			return false;
 	}
@@ -21,9 +23,29 @@ bool canFormNote(string poster, string note)
 	return true;
 }
 
-int main(int argc, char **argv)
+string showBool(bool b)
 {
-	cout << (canFormNote("AB", "A") ? "true" : "false") << endl;
+	return b ? "true" : "false";
+}
+
+int main()
+{
+	const vector< pair< pair<string, string>, bool > > v = {
+		{{"AB", "A"}, true},
+		{{"A", "AB"}, false},
+		{{"", "A"}, false},
+		{{"A", ""}, true},
+	};
+
+	for (const auto& p : v) {
+		const auto& arg_pair = p.first;
+		const string& poster = arg_pair.first;
+		const string& note = arg_pair.second;
+		const bool actual = canFormNote(poster, note);
+		const bool expected = p.second;
+
+		cout << showBool(actual) << " " << showBool(expected) << endl;
+	}
 
 	return 0;
 }
