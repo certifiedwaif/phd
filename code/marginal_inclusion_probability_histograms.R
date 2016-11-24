606d04c9e656c24e0f0f97938a94a7c8d880a33e
Mark Greenaway <markg@maths.usyd.edu.au>
2016-11-24 15:40:14 +1100

    Initial check-in

---
+++ code/marginal_inclusion_probability_histograms.R
@@ -0,0 +1,52 @@
+plot_vw <- function(vw1)
+{
+	vw1_mat <- as.matrix(vw1)
+	n <- ncol(vw1_mat)
+	r <- hist(vw1_mat, breaks=n, axes=FALSE, prob=TRUE, main="", xlab="")
+	axis(1, r$mid, as.character(1:length(r$mid)))
+}
+
+vw1 <- read.csv("Hitters_vw1.csv", header=FALSE)
+vw2 <- read.csv("Hitters_vw2.csv", header=FALSE)
+
+par(mfrow=c(2, 1))
+pdf("Hitters_histogram.pdf")
+plot_vw(vw1)
+plot_vw(vw2)
+dev.off()
+
+vw1 <- read.csv("bodyfat_vw1.csv", header=FALSE)
+vw2 <- read.csv("bodyfat_vw2.csv", header=FALSE)
+
+par(mfrow=c(2, 1))
+pdf("bodyfat_histogram.pdf")
+plot_vw(vw1)
+plot_vw(vw2)
+dev.off()
+
+vw1 <- read.csv("GradRate_vw1.csv", header=FALSE)
+vw2 <- read.csv("GradRate_vw2.csv", header=FALSE)
+
+par(mfrow=c(2, 1))
+pdf("GradRate_histogram.pdf")
+plot_vw(vw1)
+plot_vw(vw2)
+dev.off()
+
+vw1 <- read.csv("USCrime_vw1.csv", header=FALSE)
+vw2 <- read.csv("USCrime_vw2.csv", header=FALSE)
+
+par(mfrow=c(2, 1))
+pdf("USCrime_histogram.pdf")
+plot_vw(vw1)
+plot_vw(vw2)
+dev.off()
+
+vw1 <- read.csv("Wage_vw1.csv", header=FALSE)
+vw2 <- read.csv("Wage_vw2.csv", header=FALSE)
+
+par(mfrow=c(2, 1))
+pdf("Wage_histogram.pdf")
+plot_vw(vw1)
+plot_vw(vw2)
+dev.off()
