9357510b3083721f6f41a77cfbfd144ec2b4f5c2
Mark Greenaway <markg@maths.usyd.edu.au>
2017-04-19 15:50:20 +1000

    Organise more of the code into functions. Calculate median_posterior_percentage

--- code/test_cva.R
+++ code/test_cva.R
@@ -1,34 +1,41 @@
 library(correlation)
-set.seed(as.numeric(Sys.time()))
-n <- 50
-p <- 12
-sigma2 <- 1.
-mX <- matrix(rnorm(n * p), n, p)
-vy <- 5 * mX[, 1] - 10 * mX[, 3] + 0 * mX[, 12] + rnorm(n, 0., sigma2)
-mX <- scale(mX)
-vy <- (vy - mean(vy))
-vy <- sqrt(n) * vy / sqrt(sum(vy ^ 2))
-K <- 20
-initial_gamma <- matrix(rbinom(K * p, 1, .5), K, p)
-now <- Sys.time()
-cva_result <- cva(initial_gamma, vy, mX, K)
-cat(Sys.time() - now, "\n")
 library(Matrix)
-image(Matrix(cva_result$models))
-now <- Sys.time()
-corr_result <- correlation::all_correlations_mX(vy, mX)
-cat(Sys.time() - now, "\n")
-summary(corr_result$vR2)
-summary(lm(vy~mX[, 1] + mX[, 3]-1))
 
-R2 <- corr_result$vR2
-p_gamma <- corr_result$vp_gamma
-R2 <- R2[2:length(R2)]
-p_gamma <- p_gamma[2:length(p_gamma)]
-a <- 1
-b <- p
-log_p <- -n / 2 * log(1 - R2) - p_gamma / 2 * log(n) + lbeta(a + p_gamma, b + p - p_gamma)
-plot(exp(log_p), pch=24)
+# Test 1
+set.seed(as.numeric(Sys.time()))
+
+generate_data <- function()
+{
+  n <- 50
+  p <- 12
+  sigma2 <- 1.
+  mX <- matrix(0, n, p)
+  mSigma_block <- matrix(c(1, .9, .9,
+                     .9, 1, .9,
+                     .9, .9, 1), 3, 3)
+  mSigma <- as.matrix(bdiag(mSigma_block, mSigma_block, mSigma_block, mSigma_block))
+  chol_mSigma <- chol(mSigma)
+  for (i in 1:n) {
+    mX[i, ] <- t(chol_mSigma) %*% rnorm(p)
+  }
+  vy <- 1.3 * mX[, 1] + 13 * mX[, 4] + 1.3 * mX[, 7] + 1.3 * mX[, 10] + rnorm(n, 0., sigma2)
+  mX <- scale(mX)
+  vy <- (vy - mean(vy))
+  vy <- sqrt(n) * vy / sqrt(sum(vy ^ 2))
+  K <- 200
+  initial_gamma <- matrix(rbinom(K * p, 1, .5), K, p)
+  return(list(n=n, p=p, vy=vy, mX=mX, K=K, initial_gamma=initial_gamma))
+}
+data <- generate_data()
+n <- data$n
+p <- data$p
+vy <- data$vy
+mX <- data$mX
+K <- data$K
+initial_gamma <- data$initial_gamma
+
+cva_result <- cva(initial_gamma, vy, mX, K)
+corr_result <- correlation::all_correlations_mX(vy, mX)
 
 binary_to_model <- function(binary_vec)
 {
@@ -40,10 +47,62 @@ binary_to_model <- function(binary_vec)
   }
   return(acc)
 }
-cva_models <- apply(cva_result$models, 1, binary_to_model)
-points(cva_models, exp(log_p)[cva_models], pch=24, col="red")
 
-library(correlation)
+log_p <- function(n, p, vR2, vp_gamma)
+{
+  R2 <- R2[2:length(R2)]
+  p_gamma <- p_gamma[2:length(p_gamma)]
+  a <- 1
+  b <- p
+  return(-n / 2 * log(1 - R2) - p_gamma / 2 * log(n) + lbeta(a + p_gamma, b + p - p_gamma))
+}
+
+pdf("cva_low_dimensional.pdf")
+R2 <- corr_result$vR2
+p_gamma <- corr_result$vp_gamma
+plot(exp(log_p(n, p, R2, p_gamma)), pch=24, xlab="Model Index", ylab="Posterior Model Probability")
+cva_models <- apply(cva_result$models, 1, binary_to_model)
+points(cva_models, exp(log_p(n, p, R2, p_gamma))[cva_models], pch=24, col="red")
+dev.off()
+
+plot_traj_probs <- function(cva_result)
+{
+  traj_prob <- log(cva_result$trajectory_probs)
+  plot(1:ncol(traj_prob), traj_prob[1, ], ylim=c(min(traj_prob), max(traj_prob)), type="l",
+       xlab="Iteration", ylab="Posterior Probability")
+  for (k in 1:nrow(traj_prob)) {
+    lines(1:ncol(traj_prob), traj_prob[k, ])
+  }
+}
+plot_traj_probs(cva_result)
+
+median_posterior_percentage <- function(K, lambda)
+{
+  cva_results <- list()
+  for (i in 1:1e2) {
+    data <- generate_data()
+    n <- data$n
+    p <- data$p
+    vy <- data$vy
+    mX <- data$mX
+    K <- data$K
+    initial_gamma <- data$initial_gamma
+
+    cva_results[[i]] <- cva(initial_gamma, vy, mX, K, lambda=1.)
+  }
+  cva_models <- lapply(cva_results, function(x) {sort(apply(x$models, 1, function(y) {binary_to_model(y)}))})
+  vp <- exp(log_p(n, p, R2, p_gamma))
+  posterior_percentage <- sapply(cva_models, function(x) {sum(vp[x])/sum(vp)})
+  return(median(posterior_percentage))
+}
+
+for (K in c(20, 50, 100, 200)) {
+  for (lambda in 1:4) {
+    cat(K, lambda, median_posterior_percentage(20, 1), "\n")
+  }
+}
+
+# Test 2
 K <- 2
 n <- 1e3
 p <- 2
@@ -58,11 +117,9 @@ fit <- lm(vy~mX[, 1] + mX[, 2])
 initial_gamma <- matrix(c(1, 0,
                           0, 1), K, p)
 cva_result <- cva(initial_gamma, vy, mX, K, lambda=10.)
-library(Matrix)
 image(Matrix(cva_result$models))
 
 # Replicate tests from Veronika Rockova's paper
-library(correlation)
 vbeta_0 <- c(1.3, 0, 0, 1.3, 0, 0, 1.3, 0, 0, 1.3, 0, 0)
 n <- 100
 p <- length(vbeta_0)
@@ -79,6 +136,8 @@ v1=1000
 beta_init=rep(1,p)
 a=b=1
 epsilon=10^{-5}
+
+# Test 3
 library(EMVS)
 result2 <- EMVS(vy,mX,v0=v0,v1=v1,type="betabinomial",beta_init=beta_init,sigma_init=1,epsilon=epsilon,a=a,b=b)
 EMVSplot(result2,"both",FALSE)
@@ -109,8 +168,3 @@ EMVSbest(result2)
 library(Matrix)
 image(Matrix(result$models))
 
-traj_prob <- log(cva_result$trajectory_probs)
-plot(1:ncol(traj_prob), traj_prob[1, ], ylim=c(min(traj_prob), max(traj_prob)), type="l")
-for (k in 1:nrow(traj_prob)) {
-  lines(1:ncol(traj_prob), traj_prob[k, ])
-}
