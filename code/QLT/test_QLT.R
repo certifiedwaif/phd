35a4545a1a948cca9c35a1ce0d5055ad3c6b5c59
Mark Greenaway <markg@maths.usyd.edu.au>
2017-08-11 15:06:07 +1000

    Re-write program to be driven from the command line, so that I can submit more tasks to the HPC cluster in parallel

--- code/QLT/test_QLT.R
+++ code/QLT/test_QLT.R
@@ -1,28 +1,51 @@
 # test_QLT.R
 
+library(optparse)
 source("QLT.R")
 
-K <- c(1, 10, 20, 50, 100)
-data_fn <- c("generate_data_QTL", "generate_data_high_dimensional")
-start <- c("cold_start", "warm_start_covariates", "warm_start_likelihood")
-prior <- c("BIC", "ZE", "3", "4", "5", "6", "7")
-grid_df <- expand.grid(K, data_fn, start, prior)
-colnames(grid_df) <- c("K", "data_fn", "start", "prior")
 
-for (i in 1:nrow(grid_df)) {
-	K <- grid_df[i, ]$K
-	data_fn <- grid_df[i, ]$data_fn
-	start <- grid_df[i, ]$start
-	prior <- grid_df[i, ]$prior
-
-	if (data_fn == "generate_data_QTL") {
-		dat <- QLT(K, generate_data_QTL, start, prior)
-	} else {
-		dat <- QLT(K, generate_data_high_dimensional, start, prior)
-	}
-	pdf(sprintf("%s_%s_%s_%s.pdf", K, data_fn, start, prior))
-	boxplot(dat,names=c("lasso","scad","mcp",
-	# "emvs",
-	"bms","varbvs","cva"),ylim=c(0,1))
-	dev.off()
+generate_F_1_data <- function(K, data_fn, start, prior)
+{
+  if (data_fn == "generate_data_QTL") {
+    dat <- QLT(K, generate_data_QTL, start, prior)
+  } else {
+    dat <- QLT(K, generate_data_high_dimensional, start, prior)
+  }
+  write(dat, file = sprintf("results/%s_%s_%s_%s.dat", K, data_fn, start, prior))
 }
+
+
+generate_boxplots <- function(dat)
+{
+	# Base
+  pdf(sprintf("results/%s_%s_%s_%s.pdf", K, data_fn, start, prior))
+  boxplot(dat,names=c("lasso","scad","mcp",
+                      # "emvs",
+                      "bms",
+                      #"varbvs",
+                      "cva"),ylim=c(0,1))
+  dev.off()
+
+  # Ggplot
+	library(tidyverse)
+	library(forcats)
+	dat2 <- data.frame(dat)
+	colnames(dat2) <- c("lasso", "scad", "mcp", "bms", "varbvs", "cva")
+	dat3 <- dat2 %>%
+	  gather(method, F_1, lasso:cva) %>%
+	  mutate(method = parse_factor(method, c("lasso", "scad", "mcp", "bms", "varbvs", "cva")))
+	ggplot(dat3, aes(x=method, y=F_1)) + geom_boxplot() + ggtitle("Something")
+}
+
+
+main <- function()
+{
+  option_list <- list(make_option("--k", type="integer"),
+  										make_option("--data"),
+  										make_option("--prior"),
+  										make_option("--start"))
+  opt <- parse_args(OptionParser(option_list=option_list))
+  print(str(opt))
+  generate_F_1_data(opt$k, opt$data, opt$prior, opt$start)
+}
+main()
