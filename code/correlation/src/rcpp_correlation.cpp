1c4c0709c875d37bcb4d36c241294d67e10ee051
Mark Greenaway <markg@maths.usyd.edu.au>
2017-12-11 14:44:16 +1100

    Add examples, update the list of priors

--- code/correlation/src/rcpp_correlation.cpp
+++ code/correlation/src/rcpp_correlation.cpp
@@ -19,7 +19,8 @@ using namespace std;
 //' @param vy Vector of responses
 //' @param mX Covariate matrix
 //' @param g_prior The g-prior to use. The choices of g-prior available are "maruyama", "BIC", "ZE",
-//' "liang_g1", "liang_g2", "liang_g3", "robust_bayarri1" and "robust_bayarri2"
+//' "liang_g1", "liang_g2", "liang_g_n_appell", "liang_g_approx", "liang_g_n_quad",
+//' "robust_bayarri1" and "robust_bayarri2"
 //' @param intercept_col The index of the column in mX containing the intercept, if any
 //' @param bNatural_Order Whether to return the results in natural order or graycode order. Defaults to graycode order.
 //' @param bIntercept Logical value indicating whether there is an intercept column or not
@@ -61,7 +62,7 @@ using namespace std;
 //' y.t <- mD$y
 //' X.f <- data.matrix(cbind(mD[1:15]))
 //' colnames(X.f) <- varnames 
-//' corr_result <- all_correlations_mX(vy, mX, "maruyama")
+//' corr_result <- all_correlations_mX(y.t, X.f, "maruyama")
 //' 
 //' @export
 // [[Rcpp::export]]
@@ -84,7 +85,8 @@ List all_correlations_mX(NumericVector vy, NumericMatrix mX, std::string g_prior
 //' @param mX Fixed covariate matrix
 //' @param mZ Varying covariate matrix
 //' @param g_prior The g-prior to use. The choices of g-prior available are "maruyama", "BIC", "ZE",
-//' "liang_g1", "liang_g2", "liang_g3", "robust_bayarri1" and "robust_bayarri2"
+//' "liang_g1", "liang_g2", "liang_g_n_appell", "liang_g_approx", "liang_g_n_quad",
+//' "robust_bayarri1" and "robust_bayarri2"
 //' @param intercept_col The index of the column in mX containing the intercept, if any
 //' @param bNatural_Order Whether to return the results in natural order or graycode order. Defaults to graycode order.
 //' @param bIntercept Logical value indicating whether there is an intercept column or not
@@ -95,6 +97,40 @@ List all_correlations_mX(NumericVector vy, NumericMatrix mX, std::string g_prior
 //' vp_gamma, the vector of number of covariates for each model
 //' vlogp, the vector of logs of the likelihoods of each model
 //' vinclusion_prob, the vector of inclusion probabilities for each of the covariates
+//' @examples
+//' library(MASS)
+//'
+//' mD <- UScrime
+//' notlog <- c(2,ncol(UScrime))
+//' mD[,-notlog] <- log(mD[,-notlog])
+//'
+//' for (j in 1:ncol(mD)) {
+//'   mD[,j] <- (mD[,j] - mean(mD[,j]))/sd(mD[,j])
+//' }
+//'
+//' varnames <- c(
+//'   "log(AGE)",
+//'   "S",
+//'   "log(ED)",
+//'   "log(Ex0)",
+//'   "log(Ex1)",
+//'   "log(LF)",
+//'   "log(M)",
+//'   "log(N)",
+//'   "log(NW)",
+//'   "log(U1)",
+//'   "log(U2)",
+//'   "log(W)",
+//'   "log(X)",
+//'   "log(prison)",
+//'   "log(time)")
+//'
+//' y.t <- mD$y
+//' X.f <- data.matrix(cbind(mD[, 1:10]))
+//' colnames(X.f) <- varnames 
+//' Z.f <- data.matrix(cbind(mD[, 11:15]))
+//' corr_result <- all_correlations_mX_mZ(y.t, X.f, Z.f, "maruyama")
+//' 
 //' @export
 // [[Rcpp::export]]
 List all_correlations_mX_mZ(NumericVector vy, NumericMatrix mX, NumericMatrix mZ, std::string g_prior,
